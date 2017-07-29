c5a58f83c6fa53288d1e792ff73cf0ffb5c53878
Edward Louis Burgess <elburgess1@gmail.com>
2017-07-29 13:18:03 -0700

    saving current progress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/searchBar';
+import VideoList from './components/videoList';
+import VideoDetail from './components/videoDetail';
 
-const App = () => {
-  return <div>Hi!</div>
+const API_KEY = "AIzaSyA6LXM8SW4wQnko7xJR6vbgOtkfyCWRVIY";
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
